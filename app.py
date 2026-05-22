d431457d8936efe725529f033626fbb4f4372433
Shanmukhi pratapa <pratapashanumukhi@gmail.com>
2026-05-22 21:32:44 +0530

    Add Streamlit import to app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import io
 import sqlite3
 import numpy as np
 import tensorflow as tf
+import streamlit as st
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
